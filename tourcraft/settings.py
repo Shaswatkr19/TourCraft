3fcb055baeef57fd1ed95fc762591eea857afcad
shaswatkr19 <shaswatsinha000@gmail.com>
2025-08-29 11:18:03 +0530

    update some changes

--- tourcraft/settings.py
+++ tourcraft/settings.py
@@ -103,7 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
